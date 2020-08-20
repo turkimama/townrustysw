8d3f3f5eb9ade044c9d61895a5f2ac4fef75911e
turkimama <31267942+turkimama@users.noreply.github.com>
2020-08-21 02:15:32 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 
 module.exports = {
-    TOKEN: 'NzA4MzQ5Nzc4NzQzNzg3NTIw.XrWEIw.aUwTWF07YRqT1RFdF6QhiyY3UsY',    
+    TOKEN: 'NzA4MzQ5Nzc4NzQzNzg3NTIw.XrWEIw.2epdHbkXhKKXA_KACgJ5D4QaCL0',    
     YT_API_KEY: 'api ', 
     prefix: '+',
     devs: ['661738866713165893']
